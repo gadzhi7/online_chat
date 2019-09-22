63e82c8cde8c93e311eaed490784e188204fd428
gadzhi <gadzhi7.7.7@mail.ru>
2019-09-22 10:22:19 +0300

    start app

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+const http = require('http');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection', socket => {
+  console.log('IO Connection')
+
+  socket.on('createMessage', (data) => {
+    console.log('Socket:createMessage', data)
+
+    socket.emit('newMessage', {
+      text: data.value,
+      date: new Date()
+    })
+  })
+})
+
+server.listen(port, () => {
+
+  console.log(` server has been started on port ${port}`)
+})
